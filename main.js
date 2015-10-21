22719df5c7afb75d003d7167fbc6f89191c5e5f6
Maxime Nicole <maxime.nicole@icloud.com>
2015-10-22 01:22:22 +0200

    Update

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ var program = require('./program');
 var io = require('socket.io-client');
 var socket = io.connect('http://localhost:3000', {reconnect: true});
 
+
 // Add a connect listener
 socket.on('connect', function (socket) {
     console.log('Connected with Master!');
