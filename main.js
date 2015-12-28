1ccbf7ebcf915d24670e3c5cfcd5d4bf31382cb0
Maxime Nicole <maxime.nicole@icloud.com>
2015-12-28 15:28:56 +0100

    Update

--- main.js
+++ main.js
@@ -6,47 +6,54 @@ var socket = io.connect('http://localhost:3000', {reconnect: true});
 
 
 // Add a connect listener
-socket.on('connect', function (socket) {
+socket.on('connect', function (data) {
     console.log('Connected with Master!');
+    // Envoi au Master son identification
+    socket.emit('slave');
+});
+
+// Check
+socket.on('check', function () {
+    socket.emit('check-slave');
 });
 
 // Listeners
 socket.on('play', function (data) {
     if (typeof(data.timeStart) !== 'undefined') {
         console.info('play [-> timeStart]');
-        program.omx.timeStartVideo(data.timeStart, data.path, data.name);
+        //program.omx.timeStartVideo(data.timeStart, data.path, data.name);
     } else {
         console.info('play');
-        program.omx.launchVideo(data.path, data.name);
+        //program.omx.launchVideo(data.path, data.name);
     }
 });
 
 socket.on('pause', function (data) {
     console.info('pause');
-    program.omx.pauseVideo();
+    //program.omx.pauseVideo();
 });
 
 socket.on('resume', function (data) {
     console.info('resume');
-    program.omx.resumeVideo();
+    //program.omx.resumeVideo();
 });
 
 socket.on('stop', function (data) {
     console.info('stop');
-    program.omx.stopVideo();
+    //program.omx.stopVideo();
 });
 
 socket.on('status', function (data) {
     console.info('status');
-    program.omx.omxStatus();
+    //program.omx.omxStatus();
 });
 
 socket.on('upVolume', function (data) {
     console.info('upVolume');
-    program.omx.upVolumeVideo();
+    //program.omx.upVolumeVideo();
 });
 
 socket.on('downVolume', function (data) {
     console.info('downVolume');
-    program.omx.downVolumeVideo();
+    //program.omx.downVolumeVideo();
 });
